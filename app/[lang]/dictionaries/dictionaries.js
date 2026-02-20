7a466f3430030886340ce23479e854ae03d820d0
anaitullah190 <anaitullah190@gmail.com>
2026-02-20 20:16:20 +0800

    Re-deploy project

--- app/[lang]/dictionaries/dictionaries.js
+++ app/[lang]/dictionaries/dictionaries.js
@@ -10,6 +10,7 @@ export const getDictionary = async (locale) => {
   if (!locale || !dictionaries[locale]) {
     // fallback to English (or throw / notFound)
     return dictionaries.en();
+    
   }
   return dictionaries[locale]();
 };
